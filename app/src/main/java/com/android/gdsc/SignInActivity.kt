fd2173bbd2ffc496f51e9fd0909487505f8f44f0
Anushek Prasal <anushekprasal@gmail.com>
2021-12-11 05:11:03 +0530

    GDSC: Rework Sign-In activity
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

---
+++ app/src/main/java/com/android/gdsc/SignInActivity.kt
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2021 Anushek Prasal (SKULSHADY) <anushekprasal@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.gdsc
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.android.material.button.MaterialButton
+
+class SignInActivity : AppCompatActivity() {
+
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        val signInButton = findViewById<MaterialButton>(R.id.sign_in_button)
+        signInButton.setOnClickListener {
+            signIn()
+        }
+    }
+
+    private fun signIn() {
+        startActivityForResult(mGoogleSignInClient.signInIntent, 1)
+    }
+
+    private fun updateUI(account: GoogleSignInAccount?) {
+        Utils.setBooleanPreference(this, "signed_in", true)
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 1) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account: GoogleSignInAccount? = completedTask.getResult(ApiException::class.java)
+            updateUI(account)
+        } catch (e: ApiException) {
+            updateUI(null)
+        }
+    }
+}
