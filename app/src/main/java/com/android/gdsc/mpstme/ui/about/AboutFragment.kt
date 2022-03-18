7a77a8da9cc3290640454c8efbf7a9ee18306c2e
Anushek Prasal <anushekprasal@gmail.com>
2022-03-19 01:17:56 +0530

    GDSC: Add content to About fragment
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

---
+++ app/src/main/java/com/android/gdsc/mpstme/ui/about/AboutFragment.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2022 Anushek Prasal (SKULSHADY) <anushekprasal@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.gdsc.mpstme.ui.about
+
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.android.gdsc.mpstme.R
+import com.android.gdsc.mpstme.databinding.FragmentAboutBinding
+
+class AboutFragment : Fragment() {
+
+    private var _binding: FragmentAboutBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var mContext: Context
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+
+        _binding = FragmentAboutBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        mContext = requireActivity()
+
+        try {
+            val packageInfo = mContext.packageManager.getPackageInfo(mContext.packageName, 0)
+            val version = packageInfo.versionName
+            binding.version.text = version
+        } catch (e: PackageManager.NameNotFoundException) {
+        }
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
