cf6a9abf772636a24c36959ec67b749d7cc6b34c
Anushek Prasal <anushekprasal@gmail.com>
2022-03-19 01:31:37 +0530

    GDSC: Cleanup unused resources
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- app/src/main/java/com/android/gdsc/mpstme/event/EventAdapter.kt
+++ app/src/main/java/com/android/gdsc/mpstme/event/EventAdapter.kt
@@ -26,8 +26,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
-import com.android.gdsc.mpstme.ui.triumphs.PastEventsActivity
 import com.android.gdsc.mpstme.R
+import com.android.gdsc.mpstme.ui.triumphs.PastEventsActivity
 import com.android.gdsc.mpstme.ui.triumphs.TriumphsFragment
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
--- app/src/main/java/com/android/gdsc/mpstme/ui/about/AboutFragment.kt
+++ app/src/main/java/com/android/gdsc/mpstme/ui/about/AboutFragment.kt
@@ -23,7 +23,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.android.gdsc.mpstme.R
 import com.android.gdsc.mpstme.databinding.FragmentAboutBinding
 
 class AboutFragment : Fragment() {
